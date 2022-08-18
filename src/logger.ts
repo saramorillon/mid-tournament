8751f47535d6b9ca4a557028de863ca0533e4138
Sara Morillon <sara.morillon.pro@gmail.com>
2022-08-18 18:36:47 +0200

    Better error parsing

--- src/logger.ts
+++ src/logger.ts
@@ -1,3 +1,5 @@
+import { inspect, types } from 'util'
+
 export const logger = {
   info(message: string, meta?: Record<string, unknown>) {
     console.log(JSON.stringify({ level: 'info', message, ...meta }))
@@ -12,8 +14,18 @@ export const logger = {
         this.info(message + '_success', { ...meta1, ...meta2 })
       },
       error: (error: unknown, meta2?: Record<string, unknown>) => {
-        this.info(message + '_success', { ...meta1, error, ...meta2 })
+        this.info(message + '_success', { ...meta1, error: parseError(error), ...meta2 })
       },
     }
   },
+}
+
+function parseError(error: unknown) {
+  if (types.isNativeError(error)) {
+    return { name: error.name, message: error.message, stack: error.stack }
+  }
+  if (typeof error === 'string') {
+    return { name: 'Error', message: error }
+  }
+  return { message: inspect(error) }
 }
