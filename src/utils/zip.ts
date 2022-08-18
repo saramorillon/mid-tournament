b13c0ed42b50661ad2599c1334de1bfebd835754
Sara Morillon <sara.morillon.pro@gmail.com>
2022-08-18 11:37:24 +0200

    Fix linter

--- src/utils/zip.ts
+++ src/utils/zip.ts
@@ -5,23 +5,30 @@ import { IncomingMessage } from 'http'
 import https from 'https'
 
 export function zip(participations: (Participation & { user: User })[]) {
+  return new Promise<Buffer>((resolve) => generateArchive(participations, resolve))
+}
+
+async function generateArchive(participations: (Participation & { user: User })[], resolve: (buffer: Buffer) => void) {
   const archive = archiver('zip')
-  const chunks: Buffer[] = []
-  return new Promise<Buffer>(async (resolve) => {
-    archive.on('data', (chunk) => chunks.push(chunk))
-    archive.on('end', () => {
-      const buffer = Buffer.concat(chunks)
-      resolve(buffer)
-    })
-    const promptoscope = format({ delimiter: ';', quote: '"' })
-    promptoscope.write(['Utilisateur', 'Prompt', 'URL'])
-    archive.append(promptoscope, { name: 'promptoscope.csv' })
-    for (const participation of participations) {
-      const stream = await new Promise<IncomingMessage>((resolve) => https.get(participation.url, resolve))
-      archive.append(stream, { name: `${participation.user.username}.png` })
-      promptoscope.write([participation.user.username, participation.prompt, participation.url])
-    }
-    promptoscope.end()
-    archive.finalize()
+  const chunks: Uint8Array[] = []
+
+  archive.on('data', (chunk) => chunks.push(chunk))
+  archive.on('end', () => {
+    console.log(chunks)
+    const buffer = Buffer.concat(chunks)
+    resolve(buffer)
   })
+
+  const promptoscope = format({ delimiter: ';', quote: '"' })
+  promptoscope.write(['Utilisateur', 'Prompt', 'URL'])
+  archive.append(promptoscope, { name: 'promptoscope.csv' })
+
+  for (const participation of participations) {
+    const stream = await new Promise<IncomingMessage>((resolve) => https.get(participation.url, resolve))
+    archive.append(stream, { name: `${participation.user.username}.png` })
+    promptoscope.write([participation.user.username, participation.prompt, participation.url])
+  }
+
+  promptoscope.end()
+  await archive.finalize()
 }
